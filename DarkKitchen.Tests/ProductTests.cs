0c223b3f7147eab4b1f40a8c9f99fed79001cc3f
Mateo <MM242444@fi365.ort.ed.uy>
2026-04-07 11:09:00 -0300

    RED: Product validation test

--- DarkKitchen.Tests/ProductTests.cs
+++ DarkKitchen.Tests/ProductTests.cs
@@ -34,4 +34,88 @@ public class ProductTests
         Assert.AreEqual(150.50m, product.Price);
         Assert.IsTrue(product.IsActive);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithCodeTooShort_ShouldThrowException()
+    {
+        new Product("AB", "Hamburguesa", "Hamburguesa con queso cheddar", defaultLine, defaultCategory,
+            100m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithCodeTooLong_ShouldThrowException()
+    {
+        new Product("ABCDEFGHIJKLMNOPQRSTU", "Hamburguesa", "Hamburguesa con queso cheddar",
+            defaultLine, defaultCategory, 100m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithNonAlphanumericCode_ShouldThrowException()
+    {
+        new Product("ABC@!", "Hamburguesa", "Hamburguesa con queso cheddar", defaultLine,
+            defaultCategory, 100m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithNameTooShort_ShouldThrowException()
+    {
+        new Product("ABC12", "Corto", "Hamburguesa con queso cheddar", defaultLine, defaultCategory, 100m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithNameTooLong_ShouldThrowException()
+    {
+        new Product("ABC12", new string('A', 51), "Hamburguesa con queso cheddar", defaultLine, defaultCategory,
+            100m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithDescriptionTooShort_ShouldThrowException()
+    {
+        new Product("ABC12", "Hamburguesa", "Corta", defaultLine, defaultCategory, 100m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithDescriptionTooLong_ShouldThrowException()
+    {
+        new Product("ABC12", "Hamburguesa", new string('A', 501), defaultLine, defaultCategory, 100m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithNegativePrice_ShouldThrowException()
+    {
+        new Product("ABC12", "Hamburguesa", "Hamburguesa con queso cheddar", defaultLine,
+            defaultCategory, -10m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithZeroPrice_ShouldThrowException()
+    {
+        new Product("ABC12", "Hamburguesa", "Hamburguesa con queso cheddar", defaultLine,
+            defaultCategory, 0m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithNullLine_ShouldThrowException()
+    {
+        new Product("ABC12", "Hamburguesa", "Hamburguesa con queso cheddar", null, defaultCategory,
+            100m);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CreateProduct_WithNullCategory_ShouldThrowException()
+    {
+        new Product("ABC12", "Hamburguesa", "Hamburguesa con queso cheddar", defaultLine, null, 100m);
+    }
 }
