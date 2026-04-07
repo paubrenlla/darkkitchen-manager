b9d2a9f8c32d7277d322cb6e0da3dd8bd38897db
Pau Brenlla <pb311149@fi365.ort.edu.uy>
2026-04-07 13:26:53 -0300

    doc: fighting against actions

--- DarkKitchen.Domain/Users/Role.cs
+++ DarkKitchen.Domain/Users/Role.cs
@@ -1,8 +1,23 @@
 namespace DarkKitchen.Domain.Users;
 
+/// <summary>
+///     Represents the different user roles in DarkKitchen.
+/// </summary>
 public enum Role
 {
+    /// <summary>
+    ///     User that makes "Pedidos".
+    /// </summary>
     Cliente,
+
+    /// <summary>
+    ///     User that manages the "Pedidos", stock and general administration.
+    ///     Personal encargado de la gestión de pedidos, stock y administración general.
+    /// </summary>
     Administrativo,
+
+    /// <summary>
+    ///     User that makers the "Products" for "Pedidos" fisically.
+    /// </summary>
     Preparador
 }
