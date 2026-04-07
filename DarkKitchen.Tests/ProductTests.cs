d6f9185eef34b8731b24239687a9b7c7c0cbcaec
Mateo <MM242444@fi365.ort.ed.uy>
2026-04-07 10:45:31 -0300

    RED: Product creation

---
+++ DarkKitchen.Tests/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿namespace DarkKitchen.Tests;
+
+[TestClass]
+public class ProductTests
+{
+    private ProductLine defaultLine;
+    private ProductCategory defaultCategory;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        defaultLine = new ProductLine("Combo burgers");
+        defaultCategory = new ProductCategory("Parrilla");
+    }
+
+    [TestMethod]
+    public void CreateProduct_WithValidData_ShouldCreateSuccessfully()
+    {
+        var product = new Product(
+            "ABC12",
+            "Hamburguesa Clasica",
+            "Hamburguesa clasica con queso cheddar",
+            defaultLine,
+            defaultCategory,
+            150.50m);
+
+        Assert.AreEqual("ABC12", product.Code);
+        Assert.AreEqual("Hamburguesa Clasica", product.Name);
+        Assert.AreEqual("Hamburguesa clasica con queso cheddar", product.Description);
+        Assert.AreEqual(defaultLine, product.Line);
+        Assert.AreEqual(defaultCategory, product.Category);
+        Assert.AreEqual(150.50m, product.Price);
+        Assert.IsTrue(product.IsActive);
+    }
+}
