fd9842a31620f46aa0e4814f76a7e0e74b770876
Ana Barboza <anabarbozauy@gmail.com>
2026-04-08 00:43:12 -0300

    green: added validation letters in uru phone number

--- DarkKitchen.Domain/Users/UruguayPhoneValidationStrategy.cs
+++ DarkKitchen.Domain/Users/UruguayPhoneValidationStrategy.cs
@@ -1,10 +1,12 @@
+using System.Text.RegularExpressions;
+
 namespace DarkKitchen.Domain.Users;
 
 public class UruguayPhoneValidationStrategy
 {
     public bool IsValid(string phone)
     {
-        if(phone != null && phone.Length == 9 && phone.StartsWith("09"))
+        if(phone != null && phone.Length == 9 && phone.StartsWith("09") && Regex.IsMatch(phone, @"^09[1-9][0-9]{6}$"))
         {
             return true;
         }
