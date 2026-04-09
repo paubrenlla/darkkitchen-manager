7b7876d8d7f62039a30f97a793a7e778d4d415f5
Pau Brenlla <pb311149@fi365.ort.edu.uy>
2026-04-09 00:53:47 -0300

    feat: added create user dtos and updated login dto

--- DarkKitchen.Models/DTOs/LoginRequest.cs
+++ DarkKitchen.Models/DTOs/LoginRequest.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DarkKitchen.Models.DTOs;
 
-public class LoginRequest(string email, string password)
+public class LoginRequest
 {
-    public string Email { get; set; } = email;
-    public string Password { get; set; } = password;
+    [Required(ErrorMessage = "El email es obligatorio para iniciar sesión.")]
+    [EmailAddress(ErrorMessage = "El formato del email ingresado no es válido.")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "La contraseña es obligatoria para iniciar sesión.")]
+    public required string Password { get; set; }
 }
