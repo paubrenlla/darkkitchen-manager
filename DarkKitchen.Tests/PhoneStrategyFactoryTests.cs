b6388cd8c457e1c34efa39efdb44158c97618df1
Pau Brenlla <pb311149@fi365.ort.edu.uy>
2026-04-09 00:23:13 -0300

    red: add tests for phone strategy factory

---
+++ DarkKitchen.Tests/PhoneStrategyFactoryTests.cs
@@ -0,0 +1,43 @@
+using DarkKitchen.BusinessLogic.PhoneNumber;
+using DarkKitchen.Domain.Users;
+using Moq;
+
+namespace DarkKitchen.Tests;
+
+[TestClass]
+public class PhoneStrategyFactoryTests
+{
+    [TestMethod]
+    public void GetStrategy_ExistingPrefix_ReturnsCorrectStrategy()
+    {
+        var mockStrategyUy = new Mock<IPhoneValidationStrategy>();
+        mockStrategyUy.Setup(s => s.CountryPrefix).Returns("+598");
+
+        var mockStrategyAr = new Mock<IPhoneValidationStrategy>();
+        mockStrategyAr.Setup(s => s.CountryPrefix).Returns("+54");
+
+        var strategies = new List<IPhoneValidationStrategy>
+        {
+            mockStrategyUy.Object,
+            mockStrategyAr.Object
+        };
+
+        var factory = new PhoneStrategyFactory(strategies);
+
+        var result = factory.GetStrategy("+598");
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual("+598", result.CountryPrefix);
+        Assert.AreEqual(mockStrategyUy.Object, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(NotSupportedException))]
+    public void GetStrategy_NonExistingPrefix_ThrowsNotSupportedException()
+    {
+        var strategies = new List<IPhoneValidationStrategy>();
+        var factory = new PhoneStrategyFactory(strategies);
+
+        factory.GetStrategy("+1");
+    }
+}
