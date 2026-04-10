65dd3434decff82f831095b8b99a6cde13637af0
Ana Barboza <anabarbozauy@gmail.com>
2026-04-10 19:50:34 -0300

    green: delivered state cannot be turned into prepared

--- DarkKitchen.Domain/Orders/NotDeliveredState.cs
+++ DarkKitchen.Domain/Orders/NotDeliveredState.cs
@@ -3,9 +3,4 @@ namespace DarkKitchen.Domain.Orders;
 public class NotDeliveredState : BaseOrderState
 {
     public override string Name => "No entregado";
-
-    public override void Prepare(Order order)
-    {
-        order.ChangeState(new PreparedState());
-    }
 }
