c4acf8e52c5c8a38ba864d2a4ba8c46349160ae5
Ana Barboza <anabarbozauy@gmail.com>
2026-04-10 00:31:30 -0300

    feat: created class Address

---
+++ DarkKitchen.Domain/Users/Address.cs
@@ -0,0 +1,10 @@
+namespace DarkKitchen.Domain.Users;
+
+public class Address
+{
+    public string Street { get; set; }
+    public string Number { get; set; }
+    public string Apartment { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
