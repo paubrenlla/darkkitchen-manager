af98662973de47c552ef036d1588513b4ad1595c
Ana Barboza <anabarbozauy@gmail.com>
2026-04-10 19:48:35 -0300

    green: shipping state cannot turn into prepared

--- DarkKitchen.Domain/Orders/ShippingState.cs
+++ DarkKitchen.Domain/Orders/ShippingState.cs
@@ -3,9 +3,4 @@ namespace DarkKitchen.Domain.Orders;
 public class ShippingState : BaseOrderState
 {
     public override string Name => "En camino";
-
-    public override void Prepare(Order order)
-    {
-        order.ChangeState(new PreparedState());
-    }
 }
