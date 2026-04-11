0b48cc36cf9425581c1bfb4f77a349f664588201
Ana Barboza <anabarbozauy@gmail.com>
2026-04-10 22:26:38 -0300

    green: validated quantity greater than 0

--- DarkKitchen.Domain/Orders/OrderItem.cs
+++ DarkKitchen.Domain/Orders/OrderItem.cs
@@ -4,6 +4,11 @@ public class OrderItem
 {
     public OrderItem(Guid productId, int quantity, decimal price)
     {
+        if(quantity <= 0)
+        {
+            throw new ArgumentException("Quantity must be greater than zero.");
+        }
+
         ProductId = productId;
         Quantity = quantity;
         Price = price;
