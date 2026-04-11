484e9ec95d53e3f459c5ac189c976554d61b77b2
Ana Barboza <anabarbozauy@gmail.com>
2026-04-10 21:17:43 -0300

    green: not delivered cannot be cancelled

--- DarkKitchen.Domain/Orders/NotDeliveredState.cs
+++ DarkKitchen.Domain/Orders/NotDeliveredState.cs
@@ -3,9 +3,4 @@ namespace DarkKitchen.Domain.Orders;
 public class NotDeliveredState : BaseOrderState
 {
     public override string Name => "No entregado";
-
-    public override void Cancel(Order order)
-    {
-        order.ChangeState(new CancelledState());
-    }
 }
