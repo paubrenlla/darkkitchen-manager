f1f9d930c3122f8f63a42955e1600f8b122e4997
Ana Barboza <anabarbozauy@gmail.com>
2026-04-11 00:14:57 -0300

    green: current state returns same instance if no change

--- DarkKitchen.Domain/Orders/Order.cs
+++ DarkKitchen.Domain/Orders/Order.cs
@@ -3,6 +3,7 @@ namespace DarkKitchen.Domain.Orders;
 public class Order
 {
     private readonly List<OrderItem> _items = new();
+    private IOrderState? _currentState;
 
     public Order(Guid clientId, Address deliveryAddress, DeliveryType type, List<OrderItem> items)
     {
@@ -21,22 +22,32 @@ public class Order
         ChangeState(new PendingState());
     }
 
-    public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
-
     public Guid Id { get; private set; }
     public int OrderNumber { get; private set; }
     public Guid ClientId { get; private set; }
     public Address DeliveryAddress { get; private set; }
     public DeliveryType Type { get; private set; }
     public DateTime CreatedAt { get; private set; }
-
     public string StateName { get; private set; } = string.Empty;
+    public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
 
-    public IOrderState CurrentState => OrderStateFactory.Create(StateName);
+    public IOrderState CurrentState
+    {
+        get
+        {
+            if(_currentState == null)
+            {
+                _currentState = OrderStateFactory.Create(StateName);
+            }
+
+            return _currentState;
+        }
+    }
 
     public void ChangeState(IOrderState newState)
     {
         StateName = newState.Name;
+        _currentState = newState;
     }
 
     public void Prepare()
