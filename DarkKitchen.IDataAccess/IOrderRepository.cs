39f150042d9f3ed673e65e3f6451ac9590928d15
Ana Barboza <anabarbozauy@gmail.com>
2026-04-11 00:20:15 -0300

    feat: added IOrderRepository

---
+++ DarkKitchen.IDataAccess/IOrderRepository.cs
@@ -0,0 +1,11 @@
+using DarkKitchen.Domain.Orders;
+
+namespace DarkKitchen.IDataAccess;
+
+public interface IOrderRepository
+{
+    void Add(Order order);
+    Order? GetById(Guid id);
+    IEnumerable<Order> GetByClient(Guid clientId, DateTime? from = null, DateTime? to = null, string? state = null);
+    IEnumerable<Order> GetByStatus(DateTime from, DateTime to, string? state = null, string? city = null);
+}
