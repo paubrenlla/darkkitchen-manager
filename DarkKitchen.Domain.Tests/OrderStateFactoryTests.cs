9ef2a9e82c0d60894b1b2add5c64e0f137277788
Ana Barboza <anabarbozauy@gmail.com>
2026-04-10 21:44:44 -0300

    red: tests order state factory creates properly

---
+++ DarkKitchen.Domain.Tests/OrderStateFactoryTests.cs
@@ -0,0 +1,21 @@
+using DarkKitchen.Domain.Orders;
+
+namespace DarkKitchen.Tests;
+
+[TestClass]
+public class OrderStateFactoryTests
+{
+    [DataTestMethod]
+    [DataRow("Pending", typeof(PendingState))]
+    [DataRow("Prepared", typeof(PreparedState))]
+    [DataRow("OnTheWay", typeof(ShippingState))]
+    [DataRow("Delivered", typeof(DeliveredState))]
+    [DataRow("NotDelivered", typeof(NotDeliveredState))]
+    [DataRow("Cancelled", typeof(CancelledState))]
+    public void Create_WithValidStateName_ShouldReturnCorrectType(string stateName, Type expectedType)
+    {
+        var result = OrderStateFactory.Create(stateName);
+
+        Assert.IsInstanceOfType(result, expectedType);
+    }
+}
