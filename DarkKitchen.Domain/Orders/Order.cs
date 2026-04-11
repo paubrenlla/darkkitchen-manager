f49992213f23b340ed57ddcdc4c6a7d927efda01
Ana Barboza <anabarbozauy@gmail.com>
2026-04-10 23:04:38 -0300

    green: validates order isnt empty

--- DarkKitchen.Domain/Orders/Order.cs
+++ DarkKitchen.Domain/Orders/Order.cs
@@ -2,16 +2,26 @@ namespace DarkKitchen.Domain.Orders;
 
 public class Order
 {
-    public Order(Guid clientId, Address deliveryAddress, DeliveryType type)
+    private readonly List<OrderItem> _items = new();
+
+    public Order(Guid clientId, Address deliveryAddress, DeliveryType type, List<OrderItem> items)
     {
+        if(items == null || !items.Any())
+        {
+            throw new ArgumentException("El pedido debe tener al menos un producto.");
+        }
+
         Id = Guid.NewGuid();
         ClientId = clientId;
         DeliveryAddress = deliveryAddress;
         Type = type;
         CreatedAt = DateTime.Now;
+        _items = items;
 
         ChangeState(new PendingState());
     }
+
+    public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
 
     public Guid Id { get; private set; }
     public int OrderNumber { get; private set; }
