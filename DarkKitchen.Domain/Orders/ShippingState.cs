999e68a6a100243d282024c987434da890a896a8
Ana Barboza <anabarbozauy@gmail.com>
2026-04-10 21:19:02 -0300

    green: shipping state cannot be cancelled

--- DarkKitchen.Domain/Orders/ShippingState.cs
+++ DarkKitchen.Domain/Orders/ShippingState.cs
@@ -3,9 +3,4 @@ namespace DarkKitchen.Domain.Orders;
 public class ShippingState : BaseOrderState
 {
     public override string Name => "En camino";
-
-    public override void Cancel(Order order)
-    {
-        order.ChangeState(new CancelledState());
-    }
 }
