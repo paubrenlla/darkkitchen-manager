1dba481b3ca3aa1c0af2ba2c97668a4111fbe4c9
Ana Barboza <anabarbozauy@gmail.com>
2026-04-15 19:26:13 -0300

    feat: documented enum

--- DarkKitchen.Domain/Orders/DeliveryType.cs
+++ DarkKitchen.Domain/Orders/DeliveryType.cs
@@ -2,6 +2,13 @@ namespace DarkKitchen.Domain.Orders;
 
 public enum DeliveryType
 {
+    /// <summary>
+    ///     Express delivery
+    /// </summary>
     Express,
+
+    /// <summary>
+    ///     24-hours delivery
+    /// </summary>
     TwentyFourHours
 }
