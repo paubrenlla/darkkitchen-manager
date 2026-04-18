5ccb7816dcad8ff38369fccc69d69078ca5c4cc0
Mateo <MM242444@fi365.ort.ed.uy>
2026-04-17 21:32:35 -0300

    fix:login and product response

---
+++ DarkKitchen.Models/DTOs/ProductResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace DarkKitchen.Models.DTOs;
+
+public class ProductResponse
+{
+    public required string Code { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public required string Line { get; set; }
+    public required string Category { get; set; }
+}
