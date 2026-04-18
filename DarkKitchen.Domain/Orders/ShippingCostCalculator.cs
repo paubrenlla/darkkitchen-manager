ea497ac2de34b15574ddc6a0ca894ed85481dcf7
Ana Barboza <anabarbozauy@gmail.com>
2026-04-18 01:35:44 -0300

    green: shipping calculator works for express delivery

--- DarkKitchen.Domain/Orders/ShippingCostCalculator.cs
+++ DarkKitchen.Domain/Orders/ShippingCostCalculator.cs
@@ -5,4 +5,11 @@ public class ShippingCostCalculator : IShippingCostCalculator
     private readonly decimal _expressShippingCost;
     private readonly decimal _standardShippingCost;
 
+    public decimal CalculateShippingCost(DeliveryType deliveryType)
+    {
+        return deliveryType switch
+        {
+            DeliveryType.Express => _expressShippingCost,
+        };
+    }
 }
