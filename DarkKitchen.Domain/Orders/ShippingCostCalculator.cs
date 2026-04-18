571e21cd077b492c8c700d99ee53f5e64b938a01
Ana Barboza <anabarbozauy@gmail.com>
2026-04-18 01:45:22 -0300

    green: neg 24 hs shipping cost not allowed

--- DarkKitchen.Domain/Orders/ShippingCostCalculator.cs
+++ DarkKitchen.Domain/Orders/ShippingCostCalculator.cs
@@ -4,11 +4,17 @@ public class ShippingCostCalculator : IShippingCostCalculator
 {
     private readonly decimal _expressShippingCost;
     private readonly decimal _twentyFourHoursShippingCost;
+
     public ShippingCostCalculator(decimal expressShippingCost, decimal twentyFourHoursShippingCost)
     {
         if(expressShippingCost < 0)
         {
             throw new ArgumentException("Express shipping cost cannot be negative.");
+        }
+
+        if(twentyFourHoursShippingCost < 0)
+        {
+            throw new ArgumentException("24-hours shipping cost cannot be negative.");
         }
 
         _expressShippingCost = expressShippingCost;
