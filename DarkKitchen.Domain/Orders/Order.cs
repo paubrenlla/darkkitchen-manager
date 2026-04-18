3f45cabf75ec3760ef846c0b876d17de3437a88b
Ana Barboza <anabarbozauy@gmail.com>
2026-04-18 00:24:41 -0300

    feat: removed unnecessary logic from order for SRP

--- DarkKitchen.Domain/Orders/Order.cs
+++ DarkKitchen.Domain/Orders/Order.cs
@@ -17,46 +17,20 @@ public class Order
         Type = type;
         CreatedAt = DateTime.Now;
         LastTransitionDate = DateTime.Now;
-        _items = items;
         State = OrderState.Pending;
+        _items = new List<OrderItem>(items);
     }
 
     public Guid Id { get; private set; }
     public int? OrderNumber { get; private set; }
     public Guid ClientId { get; private set; }
     public Address DeliveryAddress { get; private set; }
-    public DeliveryType Type { get; private set; }
+    public DeliveryType Type { get; }
     public DateTime CreatedAt { get; private set; }
     public DateTime LastTransitionDate { get; private set; }
     public OrderState State { get; private set; }
     public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
 
-    public void Prepare()
-    {
-        OrderStateFactory.Create(State).Prepare(this);
-    }
-
-    public void Cancel()
-    {
-        OrderStateFactory.Create(State).Cancel(this);
-    }
-
-    public void Ship()
-    {
-        OrderStateFactory.Create(State).Ship(this);
-    }
-
-    public void Deliver()
-    {
-        OrderStateFactory.Create(State).Deliver(this);
-    }
-
-    public void MarkNotDelivered()
-    {
-        OrderStateFactory.Create(State).NotDelivered(this);
-    }
-
-    internal void TransitionTo(OrderState newState)
     {
         State = newState;
         LastTransitionDate = DateTime.Now;
