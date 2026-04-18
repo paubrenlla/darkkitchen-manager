598a06c55fad816b175ca7a67225575cdda890cd
Ana Barboza <anabarbozauy@gmail.com>
2026-04-18 01:41:55 -0300

    green: express neg price not allowed

--- DarkKitchen.Domain/Orders/ShippingCostCalculator.cs
+++ DarkKitchen.Domain/Orders/ShippingCostCalculator.cs
@@ -3,16 +3,25 @@ namespace DarkKitchen.Domain.Orders;
 public class ShippingCostCalculator : IShippingCostCalculator
 {
     private readonly decimal _expressShippingCost;
-    private readonly decimal _standardShippingCost;
+    private readonly decimal _twentyFourHoursShippingCost;
+    public ShippingCostCalculator(decimal expressShippingCost, decimal twentyFourHoursShippingCost)
+    {
+        if(expressShippingCost < 0)
+        {
+            throw new ArgumentException("Express shipping cost cannot be negative.");
+        }
 
         _expressShippingCost = expressShippingCost;
-        _standardShippingCost = standardShippingCost;
+        _twentyFourHoursShippingCost = twentyFourHoursShippingCost;
+    }
+
     public decimal CalculateShippingCost(DeliveryType deliveryType)
     {
         return deliveryType switch
         {
             DeliveryType.Express => _expressShippingCost,
-            DeliveryType.TwentyFourHours => _standardShippingCost,
+            DeliveryType.TwentyFourHours => _twentyFourHoursShippingCost,
+            _ => throw new ArgumentException($"Delivery type not supported: {deliveryType}")
         };
     }
 }
