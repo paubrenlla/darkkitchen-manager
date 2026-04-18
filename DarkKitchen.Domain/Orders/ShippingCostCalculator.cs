3f0fee8e0b5de301c7b4c4ff9f5a35e53344a842
Ana Barboza <anabarbozauy@gmail.com>
2026-04-18 01:36:38 -0300

    green: shipping calc works for 24 hs

--- DarkKitchen.Domain/Orders/ShippingCostCalculator.cs
+++ DarkKitchen.Domain/Orders/ShippingCostCalculator.cs
@@ -5,11 +5,14 @@ public class ShippingCostCalculator : IShippingCostCalculator
     private readonly decimal _expressShippingCost;
     private readonly decimal _standardShippingCost;
 
+        _expressShippingCost = expressShippingCost;
+        _standardShippingCost = standardShippingCost;
     public decimal CalculateShippingCost(DeliveryType deliveryType)
     {
         return deliveryType switch
         {
             DeliveryType.Express => _expressShippingCost,
+            DeliveryType.TwentyFourHours => _standardShippingCost,
         };
     }
 }
