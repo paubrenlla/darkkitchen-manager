2aafd630aad3b15bd248122ccd72659205783fa3
MateoMunizORT <MM242444@fi365.ort.edu.uy>
2026-04-19 18:02:37 -0300

    green:add order

--- DarkKitchen.DataAccess/InMemoryOrderRepository.cs
+++ DarkKitchen.DataAccess/InMemoryOrderRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace DarkKitchen.DataAccess;
+﻿using DarkKitchen.Domain.Orders;
+using DarkKitchen.IDataAccess;
 
-public class InMemoryOrderRepository
+namespace DarkKitchen.DataAccess;
+
+public class InMemoryOrderRepository : IOrderRepository
 {
-    
+    private readonly List<Order> _orders = [];
+    private int _nextOrderNumber = 1;
+
+    public void Add(Order order)
+    {
+        order.AssignOrderNumber(_nextOrderNumber++);
+        _orders.Add(order);
+    }
+
+    public void Update(Order order)
+    {
+        return;
+    }
+
+    public Order? GetById(Guid id)
+    {
+        return _orders.FirstOrDefault(o => o.Id == id);
+    }
+
+    public IEnumerable<Order> GetByClient(Guid clientId, DateTime? from = null, DateTime? to = null, string? state = null)
+    {
+        return null;
+    }
+
+    public IEnumerable<Order> GetByStatus(DateTime from, DateTime to, string? state = null, string? city = null)
+    {
+        return null;
+    }
 }
