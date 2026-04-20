c1a999455ceca6cd8fd19ea387bcd0de13bd6fd6
MateoMunizORT <MM242444@fi365.ort.edu.uy>
2026-04-19 21:30:33 -0300

    green:create order valido

--- DarkKitchen.WebApi/Controllers/OrdersController.cs
+++ DarkKitchen.WebApi/Controllers/OrdersController.cs
@@ -1,6 +1,37 @@
-﻿namespace DarkKitchen.WebApi.Controllers;
+﻿using DarkKitchen.Domain.Orders;
+using DarkKitchen.IBusinessLogic;
+using DarkKitchen.Models.Converters;
+using DarkKitchen.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class OrdersController
+namespace DarkKitchen.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class OrdersController(IOrderService orderService) : ControllerBase
 {
-    
+    private readonly IOrderService _orderService = orderService;
+
+    [HttpPost]
+    public IActionResult CreateOrder([FromBody] OrderCreateRequest request, [FromHeader(Name = "X-Client-Id")] Guid clientId)
+    {
+        Enum.TryParse<DeliveryType>(request.DeliveryType, true, out var deliveryType);
+
+        var address = new Address(
+            request.Address.Street,
+            request.Address.Number,
+            request.Address.Apartment,
+            request.Address.City,
+            request.Address.Country);
+
+        var items = request.Items.Select(i =>
+            new OrderItem(i.ProductId, i.Quantity, 0m)).ToList();
+
+        var order = _orderService.CreateOrder(clientId, address, deliveryType, items);
+        var response = Converter.ToOrderCreateResponse(order);
+
+        return StatusCode(StatusCodes.Status201Created, response);
+    }
 }
