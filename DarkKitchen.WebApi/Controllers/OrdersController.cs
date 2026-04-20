626e344b2e15056ec38f256c812a919622b19a94
MateoMunizORT <MM242444@fi365.ort.edu.uy>
2026-04-19 21:43:29 -0300

    green:create order exception

--- DarkKitchen.WebApi/Controllers/OrdersController.cs
+++ DarkKitchen.WebApi/Controllers/OrdersController.cs
@@ -17,24 +17,31 @@ public class OrdersController(IOrderService orderService) : ControllerBase
     [HttpPost]
     public IActionResult CreateOrder([FromBody] OrderCreateRequest request, [FromHeader(Name = "X-Client-Id")] Guid clientId)
     {
-        if(!Enum.TryParse<DeliveryType>(request.DeliveryType, true, out var deliveryType))
+        try
         {
-            return BadRequest(new { error = "Tipo de entrega inválido." });
+            if(!Enum.TryParse<DeliveryType>(request.DeliveryType, true, out var deliveryType))
+            {
+                return BadRequest(new { error = "Tipo de entrega inválido." });
+            }
+
+            var address = new Address(
+                request.Address.Street,
+                request.Address.Number,
+                request.Address.Apartment,
+                request.Address.City,
+                request.Address.Country);
+
+            var items = request.Items.Select(i =>
+                new OrderItem(i.ProductId, i.Quantity, 0m)).ToList();
+
+            var order = _orderService.CreateOrder(clientId, address, deliveryType, items);
+            var response = Converter.ToOrderCreateResponse(order);
+
+            return StatusCode(StatusCodes.Status201Created, response);
         }
-
-        var address = new Address(
-            request.Address.Street,
-            request.Address.Number,
-            request.Address.Apartment,
-            request.Address.City,
-            request.Address.Country);
-
-        var items = request.Items.Select(i =>
-            new OrderItem(i.ProductId, i.Quantity, 0m)).ToList();
-
-        var order = _orderService.CreateOrder(clientId, address, deliveryType, items);
-        var response = Converter.ToOrderCreateResponse(order);
-
-        return StatusCode(StatusCodes.Status201Created, response);
+        catch(ArgumentException ex)
+        {
+            return BadRequest(new { error = ex.Message });
+        }
     }
 }
