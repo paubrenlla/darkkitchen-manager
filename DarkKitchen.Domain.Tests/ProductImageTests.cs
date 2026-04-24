eaec19c6b7aa4e0b4357f9b41ed19819c575d252
Mateo <MM242444@fi365.ort.ed.uy>
2026-04-24 00:49:17 -0300

    feat: create image

---
+++ DarkKitchen.Domain.Tests/ProductImageTests.cs
@@ -0,0 +1,16 @@
+﻿using DarkKitchen.Domain;
+
+namespace DarkKitchen.Domain.Tests;
+
+[TestClass]
+public class ProductImageTests
+{
+    [TestMethod]
+    public void CreateImage_WithValidData_ShouldSucceed()
+    {
+        ProductImage image = new ProductImage("photo.jpg", 100000);
+
+        Assert.AreEqual("photo.jpg", image.FileName);
+        Assert.AreEqual(100000, image.SizeInBytes);
+    }
+}
---
+++ DarkKitchen.Domain/ProductImage.cs
@@ -0,0 +1,13 @@
+﻿namespace DarkKitchen.Domain;
+
+public class ProductImage
+{
+    public string FileName { get; private set; }
+    public long SizeInBytes { get; private set; }
+
+    public ProductImage(string fileName, long sizeInBytes)
+    {
+        FileName = fileName;
+        SizeInBytes = sizeInBytes;
+    }
+}
