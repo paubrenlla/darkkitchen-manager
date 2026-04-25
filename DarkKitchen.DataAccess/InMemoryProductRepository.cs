52fc1ca5ecd4a6d8d834b7a4beda75a81fe93ce9
Mateo <MM242444@fi365.ort.ed.uy>
2026-04-24 22:18:27 -0300

    green: get product by Id Data access

--- DarkKitchen.DataAccess/InMemoryProductRepository.cs
+++ DarkKitchen.DataAccess/InMemoryProductRepository.cs
@@ -26,4 +26,9 @@ public class InMemoryProductRepository : IProductRepository
     {
         return _products;
     }
+
+    public Product? GetById(Guid id)
+    {
+        return _products.FirstOrDefault(p => p.Id == id);
+    }
 }
