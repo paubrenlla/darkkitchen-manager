f5d323e56521aaac416c56b45fc13506ba53d98e
Ana Barboza <anabarbozauy@gmail.com>
2026-04-25 17:51:36 -0300

    green: promo request sets name as required

---
+++ DarkKitchen.Models/DTOs/PromotionCreateRequest.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DarkKitchen.Models.DTOs;
+
+public class PromotionCreateRequest
+{
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    public required string Name { get; set; }
+
+    public int DiscountPercentage { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+    public List<string> ProductCodes { get; set; } = [];
+}
