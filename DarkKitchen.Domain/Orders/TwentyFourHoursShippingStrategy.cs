04515e3e55777f71cee6665e11a6557563a8ac26
Ana Barboza <anabarbozauy@gmail.com>
2026-04-26 18:11:23 -0300

    green: can handle returns true

---
+++ DarkKitchen.Domain/Orders/TwentyFourHoursShippingStrategy.cs
@@ -0,0 +1,21 @@
+namespace DarkKitchen.Domain.Orders;
+
+public class TwentyFourHoursShippingStrategy : IShippingStrategy
+{
+    private readonly decimal _cost;
+
+    public TwentyFourHoursShippingStrategy(decimal cost)
+    {
+        _cost = cost;
+    }
+
+    public bool CanHandle(DeliveryType deliveryType)
+    {
+        return true;
+    }
+
+    public decimal Calculate()
+    {
+        return new decimal(null);
+    }
+}
