b87d12dcbea1a2d13e7fe8b0f0c63efcd7c4f276
Pau Brenlla <pb311149@fi365.ort.edu.uy>
2026-05-06 00:59:11 -0300

    red: add routing tests for new domain events

--- DarkKitchen/DarkKitchen.BusinessLogic.Tests/Events/DomainEventPublisherTests.cs
+++ DarkKitchen/DarkKitchen.BusinessLogic.Tests/Events/DomainEventPublisherTests.cs
@@ -35,4 +35,73 @@ public class DomainEventPublisherTests
 
         mockAuditRepository.Verify(r => r.Save(It.Is<AuditLog>(log => log.ChangeDescription.Contains("Name cambió de 'Old Valid Name' a 'New Valid Name'"))), Times.Once);
     }
+
+    [TestMethod]
+    public void Publish_EntityCreatedEvent_ShouldInvokeAuditObserver()
+    {
+        var mockAuditRepository = new Mock<IAuditRepository>();
+        var observer = new AuditObserver(mockAuditRepository.Object);
+        var publisher = new DomainEventPublisher(observer);
+
+        var newProduct = new Product("CODE1", "New Valid Name", "This is a valid long description", new ProductLine("Line"), new ProductCategory("Cat"), 100m, new List<ProductImage> { new ProductImage("img.jpg", 1000) });
+        typeof(Product).GetProperty("Id")!.SetValue(newProduct, Guid.NewGuid());
+
+        var domainEvent = new EntityCreatedEvent<Product>
+        {
+            EntityId = newProduct.Id,
+            EntityName = "Product",
+            ResponsibleUser = "admin@darkkitchen.com",
+            NewState = newProduct
+        };
+
+        publisher.Publish(domainEvent);
+
+        mockAuditRepository.Verify(r => r.Save(It.Is<AuditLog>(log => log.ChangeDescription.Contains("Producto creado exitosamente."))), Times.Once);
+    }
+
+    [TestMethod]
+    public void Publish_EntityDeactivatedEvent_ShouldInvokeAuditObserver()
+    {
+        var mockAuditRepository = new Mock<IAuditRepository>();
+        var observer = new AuditObserver(mockAuditRepository.Object);
+        var publisher = new DomainEventPublisher(observer);
+
+        var oldProduct = new Product("CODE1", "Old Valid Name", "This is a valid long description", new ProductLine("Line"), new ProductCategory("Cat"), 100m, new List<ProductImage> { new ProductImage("img.jpg", 1000) });
+        typeof(Product).GetProperty("Id")!.SetValue(oldProduct, Guid.NewGuid());
+
+        var domainEvent = new EntityDeactivatedEvent<Product>
+        {
+            EntityId = oldProduct.Id,
+            EntityName = "Product",
+            ResponsibleUser = "admin@darkkitchen.com",
+            OldState = oldProduct
+        };
+
+        publisher.Publish(domainEvent);
+
+        mockAuditRepository.Verify(r => r.Save(It.Is<AuditLog>(log => log.ChangeDescription.Contains("Producto dado de baja."))), Times.Once);
+    }
+
+    [TestMethod]
+    public void Publish_EntityActivatedEvent_ShouldInvokeAuditObserver()
+    {
+        var mockAuditRepository = new Mock<IAuditRepository>();
+        var observer = new AuditObserver(mockAuditRepository.Object);
+        var publisher = new DomainEventPublisher(observer);
+
+        var newProduct = new Product("CODE1", "New Valid Name", "This is a valid long description", new ProductLine("Line"), new ProductCategory("Cat"), 100m, new List<ProductImage> { new ProductImage("img.jpg", 1000) });
+        typeof(Product).GetProperty("Id")!.SetValue(newProduct, Guid.NewGuid());
+
+        var domainEvent = new EntityActivatedEvent<Product>
+        {
+            EntityId = newProduct.Id,
+            EntityName = "Product",
+            ResponsibleUser = "admin@darkkitchen.com",
+            NewState = newProduct
+        };
+
+        publisher.Publish(domainEvent);
+
+        mockAuditRepository.Verify(r => r.Save(It.Is<AuditLog>(log => log.ChangeDescription.Contains("Producto dado de alta."))), Times.Once);
+    }
 }
