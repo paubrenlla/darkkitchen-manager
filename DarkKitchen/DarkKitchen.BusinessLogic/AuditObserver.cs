9734a65bab41ecdd023dc48965e7660353ae534b
Pau Brenlla <pb311149@fi365.ort.edu.uy>
2026-05-06 19:20:04 -0300

    green: implement promotion modification audit logic in audit observer

--- DarkKitchen/DarkKitchen.BusinessLogic/AuditObserver.cs
+++ DarkKitchen/DarkKitchen.BusinessLogic/AuditObserver.cs
@@ -114,4 +114,46 @@ public class AuditObserver(IAuditRepository auditRepository)
 
         _auditRepository.Save(auditLog);
     }
+
+    public void Handle(EntityModifiedEvent<Promotion> domainEvent)
+    {
+        var sb = new StringBuilder();
+        var old = domainEvent.OldState;
+        var @new = domainEvent.NewState;
+
+        if(old.Name != @new.Name)
+        {
+            sb.AppendLine($"Name cambió de '{old.Name}' a '{@new.Name}'");
+        }
+
+        if(old.DiscountPercentage != @new.DiscountPercentage)
+        {
+            sb.AppendLine($"DiscountPercentage cambió de '{old.DiscountPercentage}' a '{@new.DiscountPercentage}'");
+        }
+
+        if(old.StartDate != @new.StartDate)
+        {
+            sb.AppendLine($"StartDate cambió de '{old.StartDate}' a '{@new.StartDate}'");
+        }
+
+        if(old.EndDate != @new.EndDate)
+        {
+            sb.AppendLine($"EndDate cambió de '{old.EndDate}' a '{@new.EndDate}'");
+        }
+
+        if(!old.Products.Select(p => p.Code).SequenceEqual(@new.Products.Select(p => p.Code)))
+        {
+            sb.AppendLine("La lista de productos de la promoción fue modificada.");
+        }
+
+        var auditLog = new AuditLog
+        {
+            EntityId = domainEvent.EntityId,
+            EntityName = domainEvent.EntityName,
+            ResponsibleUser = domainEvent.ResponsibleUser,
+            ChangeDescription = sb.ToString()
+        };
+
+        _auditRepository.Save(auditLog);
+    }
 }
