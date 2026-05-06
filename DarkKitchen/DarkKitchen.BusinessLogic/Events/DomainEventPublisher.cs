0e5e0528c3c47b6b9d38d7f0650921ba4cab15a4
Pau Brenlla <pb311149@fi365.ort.edu.uy>
2026-05-06 00:59:41 -0300

    green: implement routing for new domain events

--- DarkKitchen/DarkKitchen.BusinessLogic/Events/DomainEventPublisher.cs
+++ DarkKitchen/DarkKitchen.BusinessLogic/Events/DomainEventPublisher.cs
@@ -13,5 +13,17 @@ public class DomainEventPublisher(AuditObserver auditObserver) : IDomainEventPub
         {
             _auditObserver.Handle(productEvent);
         }
+        else if (domainEvent is EntityCreatedEvent<Product> createdEvent)
+        {
+            _auditObserver.Handle(createdEvent);
+        }
+        else if (domainEvent is EntityDeactivatedEvent<Product> deactivatedEvent)
+        {
+            _auditObserver.Handle(deactivatedEvent);
+        }
+        else if (domainEvent is EntityActivatedEvent<Product> activatedEvent)
+        {
+            _auditObserver.Handle(activatedEvent);
+        }
     }
 }
