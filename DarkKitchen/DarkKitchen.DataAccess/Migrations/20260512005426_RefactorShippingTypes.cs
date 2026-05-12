6b1729dec8ab81b243c190b4d4f1d765ec483705
Mateo <MM242444@fi365.ort.edu.uy>
2026-05-11 22:16:34 -0300

    feat: missing migration files

---
+++ DarkKitchen/DarkKitchen.DataAccess/Migrations/20260512005426_RefactorShippingTypes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DarkKitchen.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorShippingTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Orders");
+
+            migrationBuilder.CreateTable(
+                name: "ShippingTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Cost = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShippingTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShippingTypes_Name",
+                table: "ShippingTypes",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShippingTypes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
