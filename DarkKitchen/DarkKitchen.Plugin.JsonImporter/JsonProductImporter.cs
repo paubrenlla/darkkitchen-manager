5d4f2c00312bd474e92db505ecb46d6a63e34b93
Pau Brenlla <paula@anagram.dev>
2026-05-12 19:30:24 -0300

    green: implement Json product importer logic

---
+++ DarkKitchen/DarkKitchen.Plugin.JsonImporter/JsonProductImporter.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using DarkKitchen.Plugin.Contracts;
+
+namespace DarkKitchen.Plugin.JsonImporter
+{
+    public class JsonProductImporter : IProductImporter
+    {
+        public string Name => "JSON Importer";
+
+        public IEnumerable<ProductImportDto> ImportProducts(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"El archivo no fue encontrado: {filePath}");
+            }
+
+            string jsonContent = File.ReadAllText(filePath);
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var products = JsonSerializer.Deserialize<List<ProductImportDto>>(jsonContent, options);
+
+            return products ?? new List<ProductImportDto>();
+        }
+    }
+}
