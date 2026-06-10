f50787531be8f4be5fd7bf5554de57ba34095e1d
Mateo <MM242444@fi365.ort.edu.uy>
2026-06-09 21:23:18 -0300

    feat: product getAll, create, update service

---
+++ DarkKitchenUI/src/app/features/products/services/product.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { ProductResponse, ProductCreateRequest, ProductUpdateRequest } from '../models/product.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = `${environment.apiUrl}/products`;
+
+  products = signal<ProductResponse[]>([]);
+  isLoading = signal(false);
+
+  constructor(private http: HttpClient) {}
+
+  getAll(name?: string, line?: string, category?: string): Observable<ProductResponse[]> {
+    let params = new HttpParams();
+    if(name) params = params.set('name', name);
+    if(line) params = params.set('line', line);
+    if(category) params = params.set('category', category);
+
+    return this.http.get<ProductResponse[]>(this.apiUrl, { params });
+  }
+
+  create(request: ProductCreateRequest): Observable<ProductResponse> {
+    return this.http.post<ProductResponse>(this.apiUrl, request);
+  }
+
+  update(id: string, request: ProductUpdateRequest): Observable<ProductResponse> {
+    return this.http.put<ProductResponse>(`${this.apiUrl}/${id}`, request);
+  }
+}
