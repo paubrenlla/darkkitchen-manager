59c4618c1e80abbdd7a6543272ab304660ca8313
Mateo <MM242444@fi365.ort.edu.uy>
2026-06-09 21:13:13 -0300

    feat: product model

---
+++ DarkKitchenUI/src/app/features/products/models/product.model.ts
@@ -0,0 +1,36 @@
+export interface ProductResponse {
+  id: string;
+  code: string;
+  name: string;
+  description: string;
+  price: number;
+  line: string;
+  category: string;
+  images: string[];
+  isActive: boolean;
+}
+
+export interface ProductImageDto {
+  url: string;
+  sizeInBytes: number;
+}
+
+export interface ProductCreateRequest {
+  code: string;
+  name: string;
+  description: string;
+  line: string;
+  category: string;
+  price: number;
+  images: ProductImageDto[];
+}
+
+export interface ProductUpdateRequest {
+  name: string;
+  description: string;
+  line: string;
+  category: string;
+  price: number;
+  images: ProductImageDto[];
+  isActive?: boolean;
+}
