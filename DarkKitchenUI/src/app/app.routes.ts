3fec35e9bb5c3e71e476bf5bb422ccd6c9521d39
Mateo <MM242444@fi365.ort.edu.uy>
2026-06-11 19:32:35 -0300

    feat: routes

--- DarkKitchenUI/src/app/app.routes.ts
+++ DarkKitchenUI/src/app/app.routes.ts
@@ -6,42 +6,52 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () =>
-      import('./features/auth/components/login/login.component')
-        .then(m => m.LoginComponent)
+      import('./features/auth/components/login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: 'register',
     loadComponent: () =>
-      import('./features/auth/components/register/register.component')
-        .then(m => m.RegisterComponent)
+      import('./features/auth/components/register/register.component').then(
+        (m) => m.RegisterComponent,
+      ),
   },
   {
     path: 'dashboard',
     canActivate: [authGuard],
     loadComponent: () =>
-      import('./features/dashboard/components/dashboard/dashboard.component')
-        .then(m => m.DashboardComponent),
+      import('./features/dashboard/components/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      ),
     children: [
       {
         path: 'products',
         canActivate: [roleGuard(['Administrativo'])],
         loadComponent: () =>
-          import('./features/products/components/product-list/product-list.component')
-            .then(m => m.ProductListComponent)
+          import('./features/products/components/product-list/product-list.component').then(
+            (m) => m.ProductListComponent,
+          ),
+      },
+      {
+        path: 'admin-orders',
+        canActivate: [roleGuard(['Administrativo'])],
+        loadComponent: () =>
+          import('./features/orders/components/admin-orders/admin-orders.component').then(
+            (m) => m.AdminOrdersComponent,
+          ),
       },
       {
         path: 'audit-logs',
         canActivate: [roleGuard(['Administrativo'])],
         loadComponent: () =>
-          import('./features/audit-logs/components/audit-log-list/audit-log-list.component')
-            .then(m => m.AuditLogListComponent)
-      }
-    ]
+          import('./features/audit-logs/components/audit-log-list/audit-log-list.component').then(
+            (m) => m.AuditLogListComponent,
+          ),
+      },
+    ],
   },
   {
     path: '',
     redirectTo: 'login',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
-
